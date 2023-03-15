c99e827ade00732072e129f4da7f2bc3408c32d1
avexyy <113837032+avexyy@users.noreply.github.com>
2023-03-15 19:53:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.Write("Enter a string: ");
+string input = Console.ReadLine()!;
+input = input.ToLower();
+
+System.Console.Write("Enter a char: ");
+char character = char.Parse(Console.ReadLine()!);
+
+bool IsContained = Contains(input, character);
+System.Console.WriteLine(IsContained);
+
+bool Contains(string input, char character)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == character)
+        {
+            return IsContained = true;
+        }
+    }
+    return IsContained = false;;
+}
